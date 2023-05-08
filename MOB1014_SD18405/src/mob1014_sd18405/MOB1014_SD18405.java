5d900e064323ab3846ac0db2fd148a0adda6a181
huyennk6 <huyennk6@fpt.edu.vn>
2023-05-08 15:52:11 +0700

    Lam quen voi Java

---
+++ MOB1014_SD18405/src/mob1014_sd18405/MOB1014_SD18405.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mob1014_sd18405;
+
+/**
+ *
+ * @author Khanh Huyen
+ */
+public class MOB1014_SD18405 {
+
+    /**
+     * @param args the command line arguments
+     */
+    //c1: psvm + tab
+    //c2: main + tab
+    public static void main(String[] args) {
+        //sout + tab
+        System.out.println("hello world");
+        System.out.print("FPT");
+        System.out.println("Polytechnic");
+        System.out.println("Ha Noi");
+        System.out.println("Ha Noi");
+        System.out.println("Ha Noi");
+
+        //click chuột phải+ Run File: chạy CT
+        //ctrl + shift+ mũi tên lên xuống: copy code
+        //ctrl + /: comment code
+        //click chuột phải+ Format code
+        // hoặc Alf+ Shift + F
+    }
+
+}
